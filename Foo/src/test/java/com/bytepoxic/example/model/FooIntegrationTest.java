fee2db013041fda04b5336cf114c792520342dbd
Victor Alfaro <vico.zizou@gmail.com>
2013-05-20 17:23:33 -0600

    changes

---
+++ Foo/src/test/java/com/bytepoxic/example/model/FooIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.bytepoxic.example.model;
+
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Foo.class)
+public class FooIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
